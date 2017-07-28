d2c15b3a2b5f235afdeaaae0e37dfe6b12c169f4
dad7 <donald.dole@nist.gov>
2017-07-28 11:14:02 -0400

    New config files for other procedures

--- tc_tools/config.py
+++ tc_tools/config.py
@@ -9,13 +9,49 @@ def tc_calibration_config(file: Union[os.path.abspath, str]
     cfg = configparser.ConfigParser()
     if not os.path.isfile(file):
         print("Creating new calibration config file")
-        config_file = open('tc_calibration_config.ini', 'w')
-        cfg['Files'] = {'output file': 'data.csv', 'headers': 'channels'}
+        config_file = open(file, 'w')
+        cfg['Files'] = {'output file': 'cal_data.csv', 'headers': 'channels'}
         cfg["Instruments"] = {'PRT address': 'ASRL1:INSTR',
                               'DAQ address': 'GPIB0::9::INSTR',
                               'bath address': 'COM4'}
         cfg['Procedure'] = {'set points': '5 15 25 35 45 55 65 75',
                             'channels': '101 102 103'}
+        cfg.write(config_file)
+        config_file.close()
+    return cfg.read(file)
+
+def valve_calibration_config(file: Union[os.path.abspath, str]
+                             = 'valve_calibration_config.ini') -> \
+    configparser.ConfigParser:
+    cfg = configparser.ConfigParser()
+    if not os.path.isfile(file):
+        print('Creating new valve calibration config file')
+        config_file = open(file, 'w')
+        cfg['Files'] = {'output file': 'valve_cal.csv'}
+        cfg['Instruments'] = {'DAQ address': 'GPIB0::9::INSTR',
+                              'draw solenoid channel': '101',
+                              'weigh solenoid channel': '101',
+                              'flow valve channel': '101'}
+        cfg['Procedure'] = {'set points': '1 2 3 4 5 6 7 8 9 10'}
+        cfg.write(config_file)
+        config_file.close()
+    return cfg.read(file)
+
+
+def doe_test_config(file: Union[
+    os.path.abspath, str] = 'doe_test_config.ini') -> configparser.ConfigParser:
+    cfg = configparser.ConfigParser()
+    if not os.path.isfile(file):
+        print('Creating new valve calibration config file')
+        config_file = open(file, 'w')
+        cfg['Files'] = {'output file': '', 'draw data file': '',
+                        'schedule file': '', 'draw headers': '',
+                        'data headers': ''}
+        cfg['Instruments'] = {'DAQ address': 'GPIB0::9::INSTR',
+                              'power meter address': 'ASRL1::INSTR'}
+        cfg['Channels'] = {'tank thermocouples': '', 'tank inlet': '',
+                           'tank outlet': '', 'scale': '', 'weigh tank': '',
+                           'flow valve': '', 'rh sensor': ''}
         cfg.write(config_file)
         config_file.close()
     return cfg.read(file)
--- tc_tools/tc_calibration.py
+++ tc_tools/tc_calibration.py
@@ -8,21 +8,6 @@ from tc_tools.procedures import setpoint_calibration
 from tc_tools.config import tc_calibration_config
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-daq', '--daq_address', type=str, dest='daq',
-                    default='GPIB0::9::INSTR', help='VISA address of the DAQ')
-parser.add_argument('-prt', '--prt_address', type=str, dest='prt',
-                    default='ASRL1::INSTR', help='VISA address of the PRT')
-parser.add_argument('-bath', '--bath_address', type=str, dest='bath',
-                    default='COM4', help='VISA address of the bath')
-parser.add_argument('-o', '--output_file', type=str, dest='o',
-                    help='Output file name or path', default='data.csv')
-parser.add_argument('-s', '--set_points', nargs='+', dest='s',
-                    help='Set points',
-                    default=[5, 15, 25, 35, 45, 55, 65, 75])
-parser.add_argument('-c', '--channels', nargs='+', dest='c', help='Channels')
-parser.add_argument('-hd', '--headers', nargs='+', dest='hd',
-                    help='Headers for the output file in the same order as the'
-                         ' channel inputs')
 parser.add_argument('-cfg', '--config_file', dest='cfg', type=str,
                     help='Name or path of configuration file.')
 in_args = parser.parse_args()
